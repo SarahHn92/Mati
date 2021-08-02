da760f024fb01a26c3170033dec301ac6d8e38e9
Sarah Hinton <smcfarland2@gmail.com>
2021-08-02 19:40:01 +1000

    adding basic nav structure

---
+++ src/components/Nav.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+function Nav({ currentPage, handlePageChange }) {
+    return (
+        <header className="navbar">
+            <nav className="toolbar">
+                <div className="left">
+                    <div id="logo"
+                        onClick={() => handlePageChange('Home')}
+                        className={currentPage === 'Home' ? 'nav-link active' : 'nav-link'} >
+                        {<i class="fab fa-houzz"></i>}
+                    </div> 
+                    
+                </div>
+                               
+                <div className="nav-items right">
+                    <ul>
+                        <li id="signup-nav" 
+                            onClick={() => handlePageChange('Signup')}
+                            className={currentPage === 'Signup' ? 'nav-link active' : 'nav-link'}>
+                            Signup
+                        </li>
+                        <li id="login-nav"
+                            onClick={() => handlePageChange('Login')}
+                            className={currentPage === 'Login' ? 'nav-link active' : 'nav-link'}>
+                            Login
+                        </li>
+                        <li id="dash-nav" 
+                            onClick={() => handlePageChange('Dash')}
+                            className={currentPage === 'Dash' ? 'nav-link active' : 'nav-link'}>
+                            Dashboard
+                        </li>
+                    </ul>
+                </div>
+            </nav> 
+        </header>
+    )
+}
+
+export default Nav;
