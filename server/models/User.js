8f77e9752fea962e43f72a87f9e16aaed00afb04
Sarah Hinton <smcfarland2@gmail.com>
2021-08-03 21:43:30 +1000

    adding User model

--- server/models/User.js
+++ server/models/User.js
@@ -1,2 +1,46 @@
 const { Schema, model } = require('mongoose');
 const bcrypt = require('bcrypt');
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [/.+@.+\..+/, 'Must match an email address!'],
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 5,
+    },
+    notes: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Notes',
+        },
+    ],
+});
+
+// hashing the password using the salt and bcrypt hashing for security
+userSchema.pre('save', async function (next) {
+    if (this.isNew || this.isModified('password')) {
+      const saltRounds = 15;
+      this.password = await bcrypt.hash(this.password, saltRounds);
+    }
+  
+    next();
+});
+// comparing the entered password to the password in the database  
+userSchema.methods.isCorrectPassword = async function (password) {
+    return bcrypt.compare(password, this.password);
+};
+  
+const User = model('User', userSchema);
+  
+module.exports = User;
