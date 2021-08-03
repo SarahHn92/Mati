98d5d90b4855b3015ac259a4c4bf02394ffc3ead
Sarah Hinton <smcfarland2@gmail.com>
2021-08-03 22:00:55 +1000

    adding in Note schema

---
+++ server/models/Note.js
@@ -0,0 +1,19 @@
+const { Schema, model } = require('mongoose');
+
+const noteSchema = new Schema({
+    date_created: {
+        type: Date,
+        default: Date.now
+    },
+    note_body: {
+        type: String,
+        required: 'Journalling is required.',
+        minlength: 1,
+        maxlength: 500,
+        trim: true
+    }
+});
+
+const Note = model('Note', noteSchema);
+
+module.exports = Note;
