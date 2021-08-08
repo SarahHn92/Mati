a26f3aed1d11b69287972950fc26b38df48285f7
Sarah Hinton <smcfarland2@gmail.com>
2021-08-08 19:42:34 +1000

    adding .env to .gitignore

--- client/src/components/pages/Dash/DashHeader.js
+++ client/src/components/pages/Dash/DashHeader.js
@@ -3,9 +3,24 @@ import React from 'react';
 function DashHeader() {
     return (
         <>
-            <div>
-                <h1> Birth is normal and natural. </h1>
-            </div>
+            <header>
+                <div className="h1Container">
+                   <h1 className="title"> Birth is normal and natural. </h1> 
+                </div>
+                <div className="">
+                    <p>
+                        "Pregnant women deserve the right and capability to make 
+                        educated decisions about their own wellbeing. Why should 
+                        this be controversial?" -Krystal Trammell
+                    </p>
+                </div>
+                <div>
+                    <p>
+                        "If you are pregnant, do your research. Every choice you 
+                        make will have an effect on your outcome." -Unknown
+                    </p>
+                </div>
+            </header>
         </>
     )
 }
