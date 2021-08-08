fd94618897664c92a55351de27034460aa9563d6
Sarah Hinton <smcfarland2@gmail.com>
2021-08-08 20:26:32 +1000

    adding styling to homepage

--- client/src/components/pages/Dash/DashHeader.js
+++ client/src/components/pages/Dash/DashHeader.js
@@ -7,15 +7,15 @@ function DashHeader() {
                 <div className="h1Container">
                    <h1 className="title"> Birth is normal and natural. </h1> 
                 </div>
-                <div className="">
-                    <p>
+                <div className="quote1">
+                    <p className="quote1text">
                         "Pregnant women deserve the right and capability to make 
                         educated decisions about their own wellbeing. Why should 
                         this be controversial?" -Krystal Trammell
                     </p>
                 </div>
-                <div>
-                    <p>
+                <div className="quote1">
+                    <p className="quote2text">
                         "If you are pregnant, do your research. Every choice you 
                         make will have an effect on your outcome." -Unknown
                     </p>
--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const { authMiddleware } = require('./utils/auth');
 
 const db = require('./config/connection');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 const server = new ApolloServer({
@@ -32,6 +32,6 @@ app.get('*', (req, res) => {
 db.once('open', () => {
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
-    console.log(`Use GraphQL at http://127.0.0.1:${PORT}${server.graphqlPath}`);
+    console.log(`Use GraphQL at http://ocalhost:${PORT}${server.graphqlPath}`);
   });
 });
